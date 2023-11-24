828be778f52249e58124930f665010802788b4f7
Mateusz Jabłoński <mail@mateuszjablonski.com>
2023-11-24 13:35:37 +0100

    add support for images in news

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react';
 import styles from './Content.module.scss';
 import { BLOCKS, Document } from '@contentful/rich-text-types'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import Image from 'next/image';
+import prepareAssetUrl from '@utils/prepareAssetUrl';
 
 type Content = {
   title: string;
@@ -12,7 +14,19 @@ type Content = {
 const Content: FC<Content> = ({ title, content, publicationDate }) => {
   const options = {
     renderNode: {
-      [BLOCKS.PARAGRAPH]: (_, children) => <p className={styles.p}>{children}</p>
+      [BLOCKS.PARAGRAPH]: (_, children) => <p className={styles.p}>{children}</p>,
+      [BLOCKS.EMBEDDED_ASSET]: (node) => {
+        const target = node.data.target;
+        return (
+          <figure className={styles.Asset}>
+            <Image 
+              src={prepareAssetUrl(target.fields.file.url)}
+              alt={target.fields.title}
+              layout="fill"
+            />
+          </figure>
+        )
+      }
     }
   };
 
